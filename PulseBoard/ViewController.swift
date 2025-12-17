1dd77215af4f27f0ed1122ba0e86053f017babd0
89Explorer <https://github.com/89Explorer>
2025-12-17 16:35:04 +0900

    Initial Commit

---
+++ PulseBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PulseBoard
+//
+//  Created by 권정근 on 12/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
